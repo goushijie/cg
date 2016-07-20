4c0fc3a0bf0ab4e726d853b3b73f9cb5bc0c672f
goushijie <passw0rd>
2016-07-20 13:59:31 +0800

    内容丢失问题

--- src/main/java/com/wy/gj/ApplicationWebXml.java
+++ src/main/java/com/wy/gj/ApplicationWebXml.java
@@ -1,21 +1,26 @@
 package com.wy.gj;
 
-import com.wy.gj.config.DefaultProfileUtil;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+import com.wy.gj.config.DefaultProfileUtil;
 
 /**
  * This is a helper Java class that provides an alternative to creating a web.xml.
  * This will be invoked only when the application is deployed to a servlet container like Tomcat, Jboss etc.
  */
 public class ApplicationWebXml extends SpringBootServletInitializer {
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
         /**
          * set a default to use when no profile is configured.
          */
         DefaultProfileUtil.addDefaultProfile(application.application());
+
         return application.sources(CgApp.class);
     }
 }
+
+
+//~ Formatted by Jindent --- http://www.jindent.com
--- src/main/java/com/wy/gj/CgApp.java
+++ src/main/java/com/wy/gj/CgApp.java
@@ -1,11 +1,18 @@
 package com.wy.gj;
 
-import com.wy.gj.config.Constants;
-import com.wy.gj.config.DefaultProfileUtil;
-import com.wy.gj.config.JHipsterProperties;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.annotation.PostConstruct;
+
+import javax.inject.Inject;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.*;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -14,22 +21,17 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
-import java.util.Collection;
+import com.wy.gj.config.Constants;
+import com.wy.gj.config.DefaultProfileUtil;
+import com.wy.gj.config.JHipsterProperties;
 
 @ComponentScan
 @EnableAutoConfiguration(exclude = { MetricFilterAutoConfiguration.class, MetricRepositoryAutoConfiguration.class })
 @EnableConfigurationProperties({ JHipsterProperties.class, LiquibaseProperties.class })
 public class CgApp {
-
     private static final Logger log = LoggerFactory.getLogger(CgApp.class);
-
     @Inject
-    private Environment env;
+    private Environment         env;
 
     /**
      * Initializes cg.
@@ -41,14 +43,19 @@ public class CgApp {
     @PostConstruct
     public void initApplication() {
         log.info("Running with Spring profile(s) : {}", Arrays.toString(env.getActiveProfiles()));
+
         Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
-        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
-            log.error("You have misconfigured your application! It should not run " +
-                "with both the 'dev' and 'prod' profiles at the same time.");
+
+        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT)
+                && activeProfiles.contains(Constants.SPRING_PROFILE_PRODUCTION)) {
+            log.error("You have misconfigured your application! It should not run "
+                      + "with both the 'dev' and 'prod' profiles at the same time.");
         }
-        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT) && activeProfiles.contains(Constants.SPRING_PROFILE_CLOUD)) {
-            log.error("You have misconfigured your application! It should not" +
-                "run with both the 'dev' and 'cloud' profiles at the same time.");
+
+        if (activeProfiles.contains(Constants.SPRING_PROFILE_DEVELOPMENT)
+                && activeProfiles.contains(Constants.SPRING_PROFILE_CLOUD)) {
+            log.error("You have misconfigured your application! It should not"
+                      + "run with both the 'dev' and 'cloud' profiles at the same time.");
         }
     }
 
@@ -60,17 +67,20 @@ public class CgApp {
      */
     public static void main(String[] args) throws UnknownHostException {
         SpringApplication app = new SpringApplication(CgApp.class);
+
         DefaultProfileUtil.addDefaultProfile(app);
+
         Environment env = app.run(args).getEnvironment();
-        log.info("\n----------------------------------------------------------\n\t" +
-                "Application '{}' is running! Access URLs:\n\t" +
-                "Local: \t\thttp://127.0.0.1:{}\n\t" +
-                "External: \thttp://{}:{}\n----------------------------------------------------------",
-            env.getProperty("spring.application.name"),
-            env.getProperty("server.port"),
-            InetAddress.getLocalHost().getHostAddress(),
-            env.getProperty("server.port"));
 
+        log.info("\n----------------------------------------------------------\n\t"
+                 + "Application '{}' is running! Access URLs:\n\t" + "Local: \t\thttp://127.0.0.1:{}\n\t"
+                 + "External: \thttp://{}:{}\n----------------------------------------------------------",
+                 env.getProperty("spring.application.name"),
+                 env.getProperty("server.port"),
+                 InetAddress.getLocalHost().getHostAddress(),
+                 env.getProperty("server.port"));
     }
-
 }
+
+
+
